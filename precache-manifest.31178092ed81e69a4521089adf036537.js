6fb62193450e051bce783fcab36b053184fa82d5
Gvind-hub <63408287+Gvind-hub@users.noreply.github.com>
2020-06-02 23:58:25 +0300

    Updates

---
+++ precache-manifest.31178092ed81e69a4521089adf036537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1252fd36ee7347b26ceee22529c3742d",
+    "url": "/react-user-details-form-with-MUI/index.html"
+  },
+  {
+    "revision": "46c8e6af3b73bcea004a",
+    "url": "/react-user-details-form-with-MUI/static/css/main.d1bb3c2f.chunk.css"
+  },
+  {
+    "revision": "d4c426b2f840e3edbe89",
+    "url": "/react-user-details-form-with-MUI/static/js/2.c0e53bc2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-user-details-form-with-MUI/static/js/2.c0e53bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c8e6af3b73bcea004a",
+    "url": "/react-user-details-form-with-MUI/static/js/main.c2c66253.chunk.js"
+  },
+  {
+    "revision": "58b88f1c2d55aa10ac51",
+    "url": "/react-user-details-form-with-MUI/static/js/runtime-main.7bf7ab3a.js"
+  },
+  {
+    "revision": "8d8d123261d33d69f40a365d22d4c0fa",
+    "url": "/react-user-details-form-with-MUI/static/media/Spinner.8d8d1232.svg"
+  }
+]);
